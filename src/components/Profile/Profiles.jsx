764e054a654f960ec0ff5847aa65e6e4ec25e14a
Kateryna <katerynawalls@proton.me>
2024-10-22 19:43:29 -0500

    завдання Profile

---
+++ src/components/Profile/Profiles.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./Profile.module.css";
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={styles.profileCard}>
+      <div className={styles.avatarContainer}>
+        <img className={styles.image} src={image} alt="Profile avatar" />
+        <h2 className={styles.name}>{name}</h2>
+        <p className={styles.username}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.socialList}>
+        <li className={styles.socialItem}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.socialItem}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.socialItem}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
